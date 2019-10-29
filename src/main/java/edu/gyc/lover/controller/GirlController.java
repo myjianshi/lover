3ba1becf94949fff877ddd5cc529573c48285635
hijianshi <171380401@qq.com>
2019-10-29 16:20:38 +0800

    添加了jsp视图

--- src/main/java/edu/gyc/lover/controller/GirlController.java
+++ src/main/java/edu/gyc/lover/controller/GirlController.java
@@ -1,28 +1,18 @@
 package edu.gyc.lover.controller;
 
-
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import edu.gyc.lover.dao.LoverDao;
 import edu.gyc.lover.model.Lover;
 import edu.gyc.lover.service.LoverService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
-/**
- * <p>
- * 前端控制器
- * </p>
- *
- * @author ls
- * @since 2019-10-29
- */
-@RestController
-@RequestMapping("/lover")
-public class LoverController {
+@Controller
+public class GirlController {
 
     @Resource
     private LoverDao loverDao;
@@ -36,17 +26,11 @@ public class LoverController {
     }
 
     @GetMapping("all")
-    public Object all() {
+    public String all(Model model) {
         List<Lover> data = loverService.list();
-        return data;
-    }
-
-    @GetMapping("names")
-    public Object names() {
-        List<String> data = loverDao.getAllLoverNames();
-        return data;
+        model.addAttribute("list", data);
+        return "index";
     }
 
 
 }
-
